0a8e970e604f3da69e19a52a14488dffc565f7b0
John <john@johntelford.com>
2021-08-22 18:29:31 -0700

    Pages and Layouts
    Signed-off-by: John <john@johntelford.com>

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import Layout from "../components/Layout";
 
-const Index = () => <Layout>Welcome to WHATABYTE!</Layout>;
+const Index = () => <Layout>Welcome to JohnTelford.com LLC</Layout>;
 
 export default Index;
--- pages/profile.js
+++ pages/profile.js
@@ -3,4 +3,3 @@ import Layout from "../components/Layout";
 const ProfileView = props => <Layout>{`Profile View`}</Layout>;
 
 export default ProfileView;
-`
