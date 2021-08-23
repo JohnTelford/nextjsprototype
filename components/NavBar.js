bad7c56e6842ccf90e5e7844f522bf44548ee71f
John <john@johntelford.com>
2021-08-22 23:45:23 -0700

    Styling Components the Next.js Way
    Signed-off-by: John <john@johntelford.com>

--- components/NavBar.js
+++ components/NavBar.js
@@ -1,15 +1,15 @@
-const navBarStyle = {
-    backgroundColor: "red",
-    color: "white",
-    width: "100%",
-    height: "60px"
-  };
-  
-  const NavBar = () => (
-    <div className="NavBar" style={navBarStyle}>
-      NAVBAR
-    </div>
-  );
-  
-  export default NavBar;
-  
+// components/NavBar.js
+
+const NavBar = () => (
+  <div className="NavBar">
+    NAVBAR
+    <style jsx>{`
+      background-color: red;
+      color: white;
+      width: 100%;
+      height: 60px;
+    `}</style>
+  </div>
+);
+
+export default NavBar;
